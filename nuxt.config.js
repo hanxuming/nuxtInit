f0e08d46e91b3cbcc24c8dfcc9c12b792884919f
hanxuming <454396799@qq.com>
2018-03-15 09:52:10 +0800

    新增bootstrap,jquery,less

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = {
   /*
   ** Headers of the page
@@ -9,18 +10,34 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
+    script : [],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel : 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  css : ['assets/css/main.css','assets/css/reset.css','assets/css/bootstrap.min.css'],
   /*
   ** Build configuration
   */
   build: {
+    vender : ['axios'],
+    plugins : [
+       new webpack.ProvidePlugin({
+         jQuery : 'jquery',
+         $ : 'jquery',
+         'window.jQuery' : 'jquery'
+       })
+    ],
+    loaders: [
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader",
+      }
+    ],
     /*
     ** Run ESLint on save
     */
